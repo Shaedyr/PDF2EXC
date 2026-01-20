f31ae36ddb809741ac4b5d9de15ad8a290cf2522
Shaedyr <jonas95martensson@gmail.com>
2026-01-20 15:33:05 +0100

    Delete PDF-to-premade-Excel-Sheet-TESTER--main directory

--- PDF-to-premade-Excel-Sheet-TESTER--main/app_modules/Sheets/Sammendrag/Sammendrag.py
+++
@@ -1,8 +0,0 @@
-from .cell_mapping import CELL_MAP
-from app_modules.excel_filler import fill_excel
-
-filled_bytes = fill_excel(
-    template_bytes=template_bytes,
-    field_values=merged_data,
-    cell_map=CELL_MAP
-)
