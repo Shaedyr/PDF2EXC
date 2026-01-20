58a9afe22763b2da8d5af234bb3c3a82d05ada4f
Shaedyr <jonas95martensson@gmail.com>
2026-01-20 10:35:57 +0100

    Update cell_mapping.py

--- PDF-to-premade-Excel-Sheet-TESTER--main/app_modules/Sheets/Sammendrag/cell_mapping.py
+++ PDF-to-premade-Excel-Sheet-TESTER--main/app_modules/Sheets/Sammendrag/cell_mapping.py
@@ -1,1 +1,12 @@
-
+CELL_MAP = {
+    "company_name": "B3",
+    "org_number": "B4",
+    "address": "B5",
+    "post_nr": "B6",
+    "city": "B7",
+    "employees": "B8",
+    "nace_code": "B9",
+    "nace_description": "B10",
+    "homepage": "B11",
+    "revenue_2024": "B16",
+}
