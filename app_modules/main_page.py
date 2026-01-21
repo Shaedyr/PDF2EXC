a93f41cf0f3d9a3e3d94c3d4177220b339cdacfb
Shaedyr <jonas95martensson@gmail.com>
2026-01-21 13:17:03 +0100

    Update main_page.py

--- app_modules/main_page.py
+++ app_modules/main_page.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from openpyxl import load_workbook
 
 from app_modules.template_loader import load_template
-from app_modules.sheets.sammendrag.BRREG_info_getter import fetch_company_by_org, search_BRREG_live
+from app_modules.sheets.sammendrag.BRREG_info_getter import fetch_company_by_org
 from app_modules.sheets.sammendrag.BRREG_Proff_info_getter_merger import merge_company_data
 from app_modules.sheets.sammendrag.Summary_getter import generate_company_summary, place_summary
 from app_modules.pdf_parser import extract_fields_from_pdf
@@ -111,3 +111,4 @@ def run():
             excel_bytes=excel_bytes,
             company_name=merged_fields.get("company_name", "Selskap")
         )
+
